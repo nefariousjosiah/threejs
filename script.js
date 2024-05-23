3bcf127e518a53cf029fa3de854250c6d9005603
nefariousjosiah <josiahtaylr97@gmail.com>
2024-05-23 15:34:37 -0400

    remove wireframe

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene();
 //Object 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const material = new THREE.MeshBasicMaterial({ color: "red"});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
