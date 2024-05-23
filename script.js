0533e4f5bd46954fff0781332412cc34c475c7e6
nefariousjosiah <josiahtaylr97@gmail.com>
2024-05-23 17:21:05 -0400

    changed cube color to white

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene();
 //Object 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red"});
+const material = new THREE.MeshBasicMaterial({ color: "white"});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -25,7 +25,7 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 scene.add(camera);
-camera.position.z = 3
+camera.position.z = 3;
 
 // Renderer
 
