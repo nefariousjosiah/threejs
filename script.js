9f487629825a3ac0b51734523cb0760c7ba92c5e
nefariousjosiah <josiahtaylr97@gmail.com>
2024-05-23 15:34:13 -0400

    add wireframe to see position of object

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene();
 //Object 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
