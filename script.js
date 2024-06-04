8444e0352b55943587258424d87e2c7da7b4ea59
nefariousjosiah <josiahtaylr97@gmail.com>
2024-06-03 21:32:30 -0400

    add white color

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene();
 //Object 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "pink"});
+const material = new THREE.MeshBasicMaterial({ color: "white"});
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
